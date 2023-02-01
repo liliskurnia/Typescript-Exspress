ba1f9e0c8089c83d20412efeeec91afeebbc6714
Lilis Kurniasari <liliskurnia548@gmail.com>
2023-02-01 10:07:28 +0700

    user routes

--- build/routers/UserRoutes.js
+++ build/routers/UserRoutes.js
@@ -7,13 +7,14 @@ const BaseRouter_1 = __importDefault(require("./BaseRouter"));
 const AuthMiddleware_1 = require("../middlewares/AuthMiddleware");
 //controller
 const UserController_1 = __importDefault(require("../controllers/UserController"));
+const UserValidator_1 = __importDefault(require("../middlewares/UserValidator"));
 class UserRoutes extends BaseRouter_1.default {
     routes() {
         this.router.get("/", AuthMiddleware_1.auth, UserController_1.default.index);
-        this.router.post("/", UserController_1.default.create);
-        this.router.get("/:id", UserController_1.default.show);
-        this.router.put("/:id", UserController_1.default.update);
-        this.router.delete("/:id", UserController_1.default.delete);
+        this.router.post("/", AuthMiddleware_1.auth, UserValidator_1.default, UserController_1.default.create);
+        this.router.get("/:id", AuthMiddleware_1.auth, UserController_1.default.show);
+        this.router.put("/:id", AuthMiddleware_1.auth, UserValidator_1.default, UserController_1.default.update);
+        this.router.delete("/:id", AuthMiddleware_1.auth, UserController_1.default.delete);
     }
 }
 exports.default = new UserRoutes().router;
--- src/routers/UserRoutes.ts
+++ src/routers/UserRoutes.ts
@@ -3,14 +3,15 @@ import { auth } from "../middlewares/AuthMiddleware";
 
 //controller
 import UserController from "../controllers/UserController";
+import validate from "../middlewares/UserValidator";
 
 class UserRoutes extends BaseRoutes{
     public routes(): void {
         this.router.get("/", auth, UserController.index);
-        this.router.post("/", UserController.create);
-        this.router.get("/:id", UserController.show);
-        this.router.put("/:id", UserController.update);
-        this.router.delete("/:id", UserController.delete);
+        this.router.post("/", auth, validate, UserController.create);
+        this.router.get("/:id", auth, UserController.show);
+        this.router.put("/:id", auth, validate, UserController.update);
+        this.router.delete("/:id", auth, UserController.delete);
     }
 }
 
